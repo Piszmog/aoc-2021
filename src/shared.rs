0a4d912192ccac92202b99f15c3d3fc7162888f5
Piszmog <piszmogcode@gmail.com>
2021-12-01 09:21:51 -0700

    update with shared functions

--- src/shared.rs
+++ src/shared.rs
@@ -1,3 +1,21 @@
-pub fn hello_world() -> String {
-    "Hello, World!".to_string()
+use std::env;
+use std::error::Error;
+use std::time::Instant;
+
+pub fn get_args() -> Vec<String> {
+    env::args().collect()
+}
+
+pub fn get_csv_reader(file_path: &str, has_headers: bool, delimiter: u8) -> Result<csv::Reader<std::fs::File>, Box<dyn Error>> {
+    let file = std::fs::File::open(file_path)?;
+    let reader = csv::ReaderBuilder::new()
+        .has_headers(has_headers)
+        .delimiter(delimiter)
+        .from_reader(file);
+    Ok(reader)
+}
+
+pub fn print_duration(start: Instant) {
+    let duration = Instant::now().duration_since(start);
+    println!("Duration: {:?}", duration);
 }
