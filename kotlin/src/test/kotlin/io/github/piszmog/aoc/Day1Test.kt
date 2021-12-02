9699cb880e6da159db01ee577d4f9f07af9fe129
Piszmog <piszmogcode@gmail.com>
2021-12-01 23:10:20 -0700

    add function to get inputs

--- kotlin/src/test/kotlin/io/github/piszmog/aoc/Day1Test.kt
+++ kotlin/src/test/kotlin/io/github/piszmog/aoc/Day1Test.kt
@@ -6,15 +6,16 @@ import kotlin.test.assertEquals
 class Day1Test {
     @Test
     fun `Day 1 Part 1`() {
-        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-        val actual = day1Part1(input)
+        val actual = day1Part1(getInput())
         assertEquals(7, actual)
     }
 
     @Test
     fun `Day 1 Part 2`() {
-        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-        val actual = day1Part2(input)
+        val actual = day1Part2(getInput())
         assertEquals(5, actual)
     }
+
+    private fun getInput() =
+        listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
 }
