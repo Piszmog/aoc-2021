4b470a7b3480f8908e7066a2c038d7739b2abae2
Piszmog <piszmogcode@gmail.com>
2021-12-05 08:32:24 -0700

    add function to read a file

--- rust/src/shared.rs
+++ rust/src/shared.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 use std::time::Instant;
 
 pub fn get_args() -> Vec<String> {
@@ -13,6 +15,11 @@ pub fn get_csv_reader(file_path: &str, has_headers: bool, delimiter: u8) -> Resu
         .delimiter(delimiter)
         .from_reader(file);
     Ok(reader)
+}
+
+pub fn get_file_reader(file_path: &str) -> Result<BufReader<File>, std::io::Error> {
+    File::open(file_path)
+        .and_then(|f| Ok(BufReader::new(f)))
 }
 
 pub fn print_elapsed_time(start: Instant) {
